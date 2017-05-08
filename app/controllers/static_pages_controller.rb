090dc7b692aeadb3d79bfa4c60e822aaff24eae9
joshcamp503 <joshcamp503@gmail.com>
2017-05-07 20:25:56 -0700

    Set up basic layout with testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,21 @@
 class StaticPagesController < ApplicationController
 
 	def home
-    render html: "Home Page"
+    render 'static_pages/home'
 	end
+
+  def signup
+  end
+
+  def login
+  end
+
+  def about
+    render 'static_pages/about'
+  end
+
+  def contact
+    render 'static_pages/contact'
+  end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @base_title = "Social Forum"
+  end
+
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Members Only | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success    
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
 end
